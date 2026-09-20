0c0ce3f05208cf4d63057bea5c364f6028afb26e
papi zen <zentot015@gmail.com>
2026-09-20 19:18:19 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "dlouis-bot",
+  "version": "1.1.0",
+  "description": "D-LOUIS WhatsApp Bot V.1 - By Zenxy.sexx",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.0",
+    "@hapi/boom": "^10.0.1",
+    "pino": "^9.0.0",
+    "qrcode-terminal": "^0.12.0",
+    "chalk": "^5.3.0",
+    "moment-timezone": "^0.5.45",
+    "readline": "^1.3.0"
+  }
+}
